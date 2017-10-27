88e15fef4cdf21780bae7456c43ad77e35e744ce
Forrest Eastland <feastland4@gmail.com>
2017-10-26 19:27:28 -0500

    updated with some inline notes

--- twoServers.js
+++ twoServers.js
@@ -1,12 +1,18 @@
 var http = require('http');
-
+var url = require('url');
 // Server 1
 // nice server
 var PORT1 = 7000
 
 function handleRequest1(request, response) {
 	// why does it seem likt it is console.logging it twice?
+	// it is because it is getting a request for favicon, dang
 	console.log("It works, super-duper!");
+	
+	//gives you the ability to look at url returned, and the parse will list it out pretty 
+	// important for verb grabbing
+	console.log(url.parse(request.url));
+
 	response.end("You are amazing!");
 }
 
